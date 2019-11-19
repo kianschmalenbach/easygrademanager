9f33ccb965d3ce7fd335630ab319475a1a900edb
Kian Schmalenbach <kian.schmalenbach@fau.de>
2019-11-19 23:51:36 +0100

    finished Tasks API

---
+++ EasyGradeManager/Controllers/API/TasksController.cs
@@ -0,0 +1,91 @@
+﻿using EasyGradeManager.Models;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using static EasyGradeManager.Static.Authorize;
+using static System.Data.Entity.EntityState;
+
+namespace EasyGradeManager.Controllers.API
+{
+    public class TasksController : ApiController
+    {
+        private readonly EasyGradeManagerContext db = new EasyGradeManagerContext();
+
+        public IHttpActionResult GetTasks()
+        {
+            return BadRequest();
+        }
+
+        public IHttpActionResult GetTask(int id)
+        {
+            return BadRequest();
+        }
+
+        public IHttpActionResult PutTask(int id, TaskDetailDTO taskDTO)
+        {
+            User authorizedUser = GetAuthorizedUser(Request.Headers.GetCookies("user").FirstOrDefault());
+            if (authorizedUser == null || authorizedUser.GetTeacher() == null)
+                return Unauthorized();
+            Task task = db.Tasks.Find(id);
+            if (taskDTO == null || task == null || task.Assignment == null || task.Assignment.Course == null ||
+                !ModelState.IsValid)
+                return BadRequest(ModelState);
+            Course course = task.Assignment.Course;
+            if (!"Teacher".Equals(GetAccessRole(authorizedUser, course)))
+                return Unauthorized();
+            if (!taskDTO.Validate(task, task.Assignment))
+                return BadRequest();
+            taskDTO.Update(task);
+            string error = db.Update(task, Modified);
+            if (error != null)
+                return BadRequest(error);
+            return Redirect("https://" + Request.RequestUri.Host + ":" + Request.RequestUri.Port + "/Assignments/" + task.Assignment.Id);
+        }
+
+        public IHttpActionResult PostTask(TaskDetailDTO taskDTO)
+        {
+            User authorizedUser = GetAuthorizedUser(Request.Headers.GetCookies("user").FirstOrDefault());
+            if (authorizedUser == null || authorizedUser.GetTeacher() == null)
+                return Unauthorized();
+            Assignment assignment = db.Assignments.Find(taskDTO.NewAssignmentId);
+            if (!ModelState.IsValid || assignment == null || assignment.Course == null ||
+                !taskDTO.Validate(null, assignment))
+                return BadRequest();
+            if (!"Teacher".Equals(GetAccessRole(authorizedUser, assignment.Course)))
+                return Unauthorized();
+            Task task = taskDTO.Create();
+            string error = db.Update(task, Added);
+            if (error != null)
+                return BadRequest(error);
+            return Redirect("https://" + Request.RequestUri.Host + ":" + Request.RequestUri.Port + "/Assignments/" + assignment.Id);
+        }
+
+        public IHttpActionResult DeleteTask(int id)
+        {
+            User authorizedUser = GetAuthorizedUser(Request.Headers.GetCookies("user").FirstOrDefault());
+            if (authorizedUser == null)
+                return Unauthorized();
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+                return NotFound();
+            if (task.Assignment == null || task.Assignment.Course == null)
+                return BadRequest();
+            if (!"Teacher".Equals(GetAccessRole(authorizedUser, task.Assignment.Course)))
+                return Unauthorized();
+            int assignmentId = task.Assignment.Id;
+            string error = db.Update(task, Deleted);
+            if (error != null)
+                return BadRequest(error);
+            return Redirect("https://" + Request.RequestUri.Host + ":" + Request.RequestUri.Port + "/Assignments/" + assignmentId);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- EasyGradeManager/Models/Task.cs
+++ EasyGradeManager/Models/Task.cs
@@ -66,5 +66,47 @@ namespace EasyGradeManager.Models
             }
         }
         public double Score { get; set; }
+        public int NewAssignmentId { get; set; }
+        public override bool Equals(object other)
+        {
+            return other != null && other is TaskDetailDTO && Id == ((TaskDetailDTO)other).Id;
+        }
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+        public bool Validate(Task task, Assignment assignment)
+        {
+            if (assignment == null || assignment.Tasks == null)
+                return false;
+            HashSet<int> numbers = new HashSet<int>();
+            HashSet<string> names = new HashSet<string>();
+            foreach (Task otherTask in assignment.Tasks)
+            {
+                numbers.Add(otherTask.Number);
+                names.Add(otherTask.Name);
+            }
+            if(task != null)
+            {
+                numbers.Remove(task.Number);
+                names.Remove(task.Name);
+            }
+            return 
+                (task == null || NewAssignmentId == 0) && !numbers.Contains(Number) && !names.Contains(Name) && 
+                MaxScore > 0;
+        }
+        public void Update(Task task)
+        {
+            task.MaxScore = MaxScore;
+            task.Name = Name;
+            task.Number = Number;
+        }
+        public Task Create()
+        {
+            Task task = new Task();
+            Update(task);
+            task.AssignmentId = NewAssignmentId;
+            return task;
+        }
     }
 }
